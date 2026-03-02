f70c66743b79663687532cbdfb037d1fd9f8c9e0
suman <sharmasuman0021@gmail.com>
2026-03-02 11:26:26 +0530

    changes

---
+++ fileio.py
@@ -0,0 +1,10 @@
+f1=open("c:/users/user/Desktop/MTM/abc.txt","r")
+data=f1.read()
+print(data)
+f1=open("c:/users/user/Desktop/MTM/abc.txt","a")
+f1.write("\n then i am planning to go outside country european")
+f1=open("c:/users/user/Desktop/MTM/abc.txt","w")
+f1.write("clar all data i will preape another file")
+
+
+
